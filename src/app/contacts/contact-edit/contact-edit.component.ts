4eb044eeaa9c8d9daf09a5ecf807f562e322c9c8
Frendlyindy <frendlyindian@gmail.com>
2023-07-15 22:54:57 -0600

    week 08

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-edit',
   templateUrl: './contact-edit.component.html',
-  styleUrls: ['./contact-edit.component.css']
+  styleUrls: ['./contact-edit.component.css'],
 })
 export class ContactEditComponent {
-
+  onCancel() {
+    throw new Error('Method not implemented.');
+  }
+  onRemoveItem(_t27: number) {
+    throw new Error('Method not implemented.');
+  }
+  groupContacts: any;
 }
--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-document-edit',
   templateUrl: './document-edit.component.html',
-  styleUrls: ['./document-edit.component.css']
+  styleUrls: ['./document-edit.component.css'],
 })
 export class DocumentEditComponent {
-
+  onCancel() {
+    throw new Error('Method not implemented.');
+  }
 }
